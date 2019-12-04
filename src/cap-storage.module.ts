975824f5939649398a33c95d20c4d39187380c57
Diego SanMo <diego14290764@itcg.edu.mx>
2019-12-04 16:12:31 -0600

    The package Json was updated

--- src/cap-storage.module.ts
+++ src/cap-storage.module.ts
@@ -11,36 +11,36 @@ import { CapFileUploadDragDropComponent } from './components/photo-upload/cap-fi
 import { NgxFileDropModule } from 'ngx-file-drop';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        NgxFileDropModule
-    ],
-    exports: [
-        CommonModule,
-        CapFileUploadComponent,
-        CapShowImageComponent,
-        CapFileUploadDragDropComponent
-    ],
-    declarations: [
-        CapFileUploadComponent,
-        CapShowImageComponent,
-        CapFileUploadDragDropComponent
-    ],
-    providers: [StorageService],
-    schemas: [
-        CUSTOM_ELEMENTS_SCHEMA
-    ]
+  imports: [
+    CommonModule,
+    NgxFileDropModule
+  ],
+  exports: [
+    CommonModule,
+    CapFileUploadComponent,
+    CapShowImageComponent,
+    CapFileUploadDragDropComponent
+  ],
+  declarations: [
+    CapFileUploadComponent,
+    CapShowImageComponent,
+    CapFileUploadDragDropComponent
+  ],
+  providers: [StorageService],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
-export class CapStorageAWS { 
-    static forRoot(config:IConfigService): ModuleWithProviders{
-       return {
-           ngModule: CapStorageAWS,
-           providers: [
-                {
-                    provide: ConfigService,
-                    useValue: config
-                }
-            ]
-       }
+export class CapStorageAWS {
+  static forRoot(config: IConfigService): ModuleWithProviders {
+    return {
+      ngModule: CapStorageAWS,
+      providers: [
+        {
+          provide: ConfigService,
+          useValue: config
+        }
+      ]
     }
+  }
 }
