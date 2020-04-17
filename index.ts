8a74d3d0f727741079d12c7a5356ca2e80070c06
Diego SanMo <diego14290764@itcg.edu.mx>
2020-04-17 12:59:24 -0500

    The interfaces file was exported.

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export * from './src/services/config-general.service';
 export * from './src/services/storage.service';
 export * from './src/components/photo-upload/cap-file-upload.component'
 export * from './src/components/show-photos/cap-show-image.component'
+export * from './src/interfaces/interface';
