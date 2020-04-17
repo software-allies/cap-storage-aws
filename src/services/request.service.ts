f62c9ec8bf48d865bc9c674e144801af2a661b9e
Diego SanMo <diego14290764@itcg.edu.mx>
2020-04-17 13:03:23 -0500

    The request services was updated. The postRequest function and the alert notification were added.

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -2,58 +2,66 @@ import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 import { ConfigService } from '../services/config-general.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { NotificationsService } from 'angular2-notifications';
-
 import Swal from 'sweetalert2';
 
 
 @Injectable({ providedIn: 'root' })
 export class RequestService {
 
-  constructor(private http: HttpClient, private _config: ConfigService, private _toast: NotificationsService) {}
+  token: string = '';
+  dataPost: any = {};
 
-  createFileRecord(dataFile: any, fields: any, credentials?: any) {
-    let dataPost: any = {};
+  constructor(private http: HttpClient, private _config: ConfigService) { }
+
+
+  async createFileRecord(dataFile: any, fields: any, token?: any) {
+
     fields.forEach((field: any) => {
       switch (field.association) {
         case 'id':
-          dataPost[`${field.name}`] = uuidv4();
+          this.dataPost[`${field.name}`] = uuidv4();
           break;
         case 'name':
-          dataPost[`${field.name}`] = dataFile.key.split('/')[1].split('.')[0];
+          this.dataPost[`${field.name}`] = dataFile.key.split('/')[1].split('.')[0];
           break;
         case 'url':
-          dataPost[`${field.name}`] = dataFile.Location;
+          this.dataPost[`${field.name}`] = dataFile.Location;
           break;
         default:
-          dataPost[`${field.name}`] = field.value;
+          this.dataPost[`${field.name}`] = field.value;
           break;
       }
     });
 
-    if(credentials.token !== ''){
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'content-type': 'application/json',
-          'Authorization': `Bearer ${credentials.token}`
-        })
-      };
-      this.http.post(`${this._config.endpoint}`, dataPost, httpOptions).subscribe((data: any) => {
-        Swal.fire(
-          'Successful!',
-          'The information was successfully saved!',
-          'success'
-        );
-        return data
-      }, (error) => {
-        console.log(error);
-        Swal.fire(
-          `${error.statusText} status ${error.status}`,
-          `${error.message}`,
-          'error'
-        );
-        return error;
-      });
+    if (token !== '') {
+      this.token = token;
+      return await this.postRequest();
     }
+  }
+
+  postRequest() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${this.token}`
+      })
+    };
+    this.http.post(`${this._config.endpoint}`, this.dataPost, httpOptions).subscribe((response: any) => {
+      Swal.fire(
+        'Successful!',
+        'The file has been saved.',
+        'success'
+      );
+      return response;
+    }, (error) => {
+      Swal.fire(
+        `${error.statusText}`,
+        `${error.error.error.message}`,
+        'error'
+      );
+      console.log('error: ', error);
+      return error;
+    });
+
   }
 }
