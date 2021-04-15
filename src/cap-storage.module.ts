6fc312b2228f84fc17c136cedfe686516f6d605c
Diego SanMo <diego14290764@itcg.edu.mx>
2021-04-15 16:48:06 -0500

    The new button component has been added.

--- src/cap-storage.module.ts
+++ src/cap-storage.module.ts
@@ -1,38 +1,51 @@
+// Angular's import
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { CommonModule } from '@angular/common';
 
+// Services
 import { StorageService } from './services/storage.service';
 import { ConfigService } from './services/config-general.service';
 
+// Components
 import { CapFileUploadComponent } from './components/photo-upload/cap-file-upload.component';
 import { CapShowImageComponent } from './components/show-photos/cap-show-image.component';
 import { CapFileUploadDragDropComponent } from './components/photo-upload/cap-file-upload-drag-drop.component';
+import { CapFileUploadButtonComponent } from './components/photo-upload/cap-file-upload-button.component';
 
+// Libraries
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { HttpClientModule } from '@angular/common/http';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { NgxPaginationModule } from 'ngx-pagination';
 
+// Interfaces
 import { awsCredentials } from './interfaces/interface';
 
+// Modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
+    NgxPaginationModule,
     CommonModule,
     NgxFileDropModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PdfViewerModule
   ],
   exports: [
     CommonModule,
     CapFileUploadComponent,
     CapShowImageComponent,
-    CapFileUploadDragDropComponent
+    CapFileUploadDragDropComponent,
+    CapFileUploadButtonComponent
   ],
   declarations: [
     CapFileUploadComponent,
     CapShowImageComponent,
-    CapFileUploadDragDropComponent
+    CapFileUploadDragDropComponent,
+    CapFileUploadButtonComponent
   ],
   providers: [StorageService],
   schemas: [
